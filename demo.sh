0826c32da20b9983a7cdaea9463c3d9535f190f1
Andrew Jeffery <andrew@aj.id.au>
2017-02-23 10:21:45 +1030

    demo: Also show that the initial switch from always-inline to inline breaks

--- demo.sh
+++ demo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+read -p "Show opt pass set 15 fail then pass? (Enter)"
 ./compile-command.sh 15-broken
 read -p "Continue? (Enter) "
 ./compile-command.sh 15
+read -p "Show opt pass set 1 fail then pass? (Enter)"
+./compile-command.sh 01-broken
+read -p "Continue? (Enter) "
+./compile-command.sh 01
