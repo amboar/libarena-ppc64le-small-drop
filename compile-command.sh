9c13459cfdf6196c452a18286f3f4a8d4cd7664d
Andrew Jeffery <andrew@aj.id.au>
2017-02-23 09:39:04 +1030

    fix absolute path in rustc invocation

--- compile-command.sh
+++ compile-command.sh
@@ -10,7 +10,7 @@ SRC=${SRC:-./arena.bc}
 BIN=${BIN:-./arena}
 ITER=${1-0}
 
-$RUSTC --crate-name arena lib.rs --emit=dep-info,link --test -C metadata=75d30fcb0454b8ed --out-dir . -L dependency=/home/arj/small-drop/target/release/deps --emit llvm-bc
+$RUSTC --crate-name arena lib.rs --emit=dep-info,link --test -C metadata=75d30fcb0454b8ed --out-dir . -L dependency=./target/release/deps --emit llvm-bc
 
 P0="$(grep -v '^#' p0.0 | tr "$'\012'" " ")"
 P1="$(grep -v '^#' p1.${ITER} | tr "$'\012'" " ")"
