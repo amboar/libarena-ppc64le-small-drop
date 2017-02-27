9001dbbd0bb394e287fc2e0c5f8cd5c41bc10157
Andrew Jeffery <andrew@aj.id.au>
2017-02-27 13:54:46 +1030

    demo.sh: Show 'inline' immediately followed by 'instcombine' causes the break

--- demo.sh
+++ demo.sh
@@ -8,9 +8,15 @@ read -p "Show test fail then pass with opt pass-set 1 ? (Enter)"
 ./compile-command.sh 01-broken
 read -p "Continue? (Enter) "
 ./compile-command.sh 01
-read -p "Show test fail then pass by dropping the instcombine subsequent to inline? (Enter)"
+read -p "Show test fail then pass by dropping the instcombine subsequent to the inline pass? (Enter)"
 ./opt-link.sh $(cat p1.upper-broken | grep -v '^#' | tr "$'\012'" " ")
 ./arena
 read -p "Continue? (Enter) "
 ./opt-link.sh $(cat p1.pre-upper-broken | grep -v '^#' | tr "$'\012'" " ")
+./arena
+read -p "Show test fail then pass by removing inline when instcombine immediately follows inline pass (Enter)"
+./opt-link.sh $(cat p1.lower-broken | grep -v '^#' | tr "$'\012'" " ")
+./arena
+read -p "Continue? (Enter) "
+./opt-link.sh $(cat p1.lower | grep -v '^#' | tr "$'\012'" " ")
 ./arena
