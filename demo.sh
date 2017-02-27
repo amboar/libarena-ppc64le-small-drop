006305bf936ffda3a35205de9d9fdb11f5f0933b
Andrew Jeffery <andrew@aj.id.au>
2017-02-27 13:39:14 +1030

    demo.sh: Reword, add instcombine test case

--- demo.sh
+++ demo.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-read -p "Show opt pass set 15 fail then pass? (Enter)"
+read -p "Show test fail then pass with opt pass-set 15 ? (Enter)"
 ./compile-command.sh 15-broken
 read -p "Continue? (Enter) "
 ./compile-command.sh 15
-read -p "Show opt pass set 1 fail then pass? (Enter)"
+read -p "Show test fail then pass with opt pass-set 1 ? (Enter)"
 ./compile-command.sh 01-broken
 read -p "Continue? (Enter) "
 ./compile-command.sh 01
+read -p "Show test fail then pass by dropping the instcombine subsequent to inline? (Enter)"
+./opt-link.sh $(cat p1.upper-broken | grep -v '^#' | tr "$'\012'" " ")
+./arena
+read -p "Continue? (Enter) "
+./opt-link.sh $(cat p1.pre-upper-broken | grep -v '^#' | tr "$'\012'" " ")
+./arena
